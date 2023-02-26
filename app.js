2c8b2c91b4c2b07571781e4895136522254c9bba
suhilx3577 <suhilx3577@gmail.com>
2023-02-26 22:13:37 +0530

    completed ch3e

--- app.js
+++ app.js
@@ -6,7 +6,10 @@ import ReactDOM from "react-dom/client";
 // root.render(heading)
 
 const heading1 = React.createElement("h1", {}, "Heading 1 from React");
-const heading2 = React.createElement("h2", {}, "Heading 2 from React");
+// console.log(heading1)
+
+const heading2 = (<h1 key="h1" id="title">Heading From JSX</h1>);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 const container = React.createElement("div", {}, [heading1, heading2]);
