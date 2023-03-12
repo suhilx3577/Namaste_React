44114ae3fc4f8f23b1387c80019b21bc324d61d1
suhilx3577 <suhilx3577@gmail.com>
2023-03-13 01:40:46 +0530

    structured by moving code to seperate folder

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,14 +6,12 @@ const NavItems =()=> (
         <h3>CONTACT</h3>
     </div>
 );
-
 export const Heading =()=> (
     <>
         <div className="header">
             <img className= "logo"
             src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png?nwm=1&nws=1&industry=food&sf="
             alt="logo"/>
-
             <NavItems/>
         </div>
     </>
